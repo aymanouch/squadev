916281e457cbcd5de9e54971e62f497487951899
aymanouch <aymanouchedih1997@gmail.com>
2021-10-05 16:34:19 -0400

    finish this webiste

--- javascript/index.js
+++ javascript/index.js
@@ -4,3 +4,28 @@ iconTop.addEventListener("click", function(){
     gsap.to('html, body', {scrollTop:0, duration:1.5});
 
 });
+
+const linksTop = document.getElementsByClassName("linksTop");
+for(let i = 0; i<linksTop.length; i++) {
+    linksTop[i].addEventListener("click", function() {
+        console.log(`#${this.getAttribute("data-scroll")}`);    
+        const whereIgo = document.getElementById(`${this.getAttribute("data-scroll")}`);
+        console.log(whereIgo);
+        gsap.to('html, body', {scrollTop:whereIgo.offsetTop, duration:1.5});
+        if(window.innerWidth <=780) {
+            animeIcon(theSwitch);
+            theSwitch = !theSwitch;
+            showHideNavbar();
+        }
+        
+    });
+}
+function getDateNow() {
+    const d=new Date();
+    let dayName = d.toLocaleDateString('locale', { weekday: 'long' });
+    let hourNow = d.getHours();
+    let minuteNow = d.getMinutes();
+    console.log(`${dayName} ${hourNow}:${minuteNow}`);
+    document.getElementById('lastDate').innerText= `${dayName} ${hourNow}:${minuteNow}`;
+}
+getDateNow();
--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,11 +1,15 @@
 const contBtn = document.getElementById("controller-icon");
 const linkList = document.getElementById("link-list");
 const timelineNav = gsap.timeline();
+function showHideNavbar() {
+   if(!theSwitch) {
+      timelineNav.to(linkList, {height:"85vh"})
+      .from("#link-list ul li", {x:-50, opacity:0, stagger:{each:.5, amount:1, from:"end"}});
+   }else{
+      timelineNav.to(linkList, {height:"0vh"});
+   }
+  
+}
 contBtn.addEventListener("click", ()=>{
-     if(!theSwitch) {
-        timelineNav.to(linkList, {height:"85vh"})
-        .from("#link-list ul li", {x:-50, opacity:0, stagger:{each:.5, amount:1, from:"end"}});
-     }else{
-        timelineNav.to(linkList, {height:"0vh"});
-     }
+   showHideNavbar();
 });
